ff90b49d15167d9d664122fec5f997c6e59f9a64
Andres Loachamin <andresloa.al@gmail.com>
2021-12-14 15:48:41 -0500

    asd

--- archivos/code2.py
+++ archivos/code2.py
@@ -1,8 +1,1 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        dic = {}
-        for i, num in enumerate(nums):
-            if num in dic:
-                return [dic[num], i]
-            else:
-                dic[target - num] = i                  
+                          
