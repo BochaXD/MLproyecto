1c32cd984eff5545c6530156db8df46ef7653afd
Andres Loachamin <andresloa.al@gmail.com>
2021-12-14 14:45:48 -0500

    commit masters

--- archivos/code1.py
+++ archivos/code1.py
@@ -1,3 +1,6 @@
-def _get_nodes(self):
-        node1, node2 = ast.parse(self._code1), ast.parse(self._code2)
-        return node1, node2         
+def _jaccard_distance(self):
+    #Distancia de Jaccard
+    node_list1, node_list2 = self._get_node_lists()
+    node_set1 = set(node_list1)
+    node_set2 = set(node_list2)
+    return 1.0 * len(node_set1 & node_set2) / len(node_set1 | node_set2)
--- archivos/code2.py
+++ archivos/code2.py
@@ -1,3 +1,33 @@
-def delete_all():
-    todos.delete_many({})
-    return redirect(url_for('index'))                        
+def _fake_anti_uni_distance(node1, node2):
+    stack1 = deque([node1])
+    stack2 = deque([node2])
+    same = 0
+    diff = 0
+    while stack1 or stack2:
+        if stack1:
+            _node1 = stack1.popleft()
+            if type(_node1).__name__ == 'Load':
+                try:
+                    _node1 = stack1.popleft()
+                except IndexError:
+                    _node1 = None
+        else:
+            _node1 = None
+        if stack2:
+            _node2 = stack2.popleft()
+            if type(_node2).__name__ == 'Load':
+                try:
+                    _node2 = stack2.popleft()
+                except IndexError:
+                    _node2 = None
+        else:
+            _node2 = None
+        if type(_node1).__name__ == type(_node2).__name__:
+            same += 1
+        else:
+            diff += 1
+        if _node1 and _node2:
+            stack1.extend(ast.iter_child_nodes(_node1))
+            stack2.extend(ast.iter_child_nodes(_node2))
+    return 1.0 * same / (same + diff)
+       
